4478b1de4611e25d2c9a720bb8e87af12bcd4a1a
Keng-Yap <kengyap123@gmail.com>
2025-12-15 16:04:14 +0800

    new update

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+public items: any[] = [
+    { time: '8 hours ago', title: 'Auto model selection is generally available in…' },
+    { time: '10 hours ago', title: 'GitHub Spark: Improvements, DPA…' },
+    { time: '11 hours ago', title: 'GitHub Enterprise Server 3.19 is now generally…' },
+    { time: '16 hours ago', title: 'The GitHub MCP Server adds support for tool…' },
+    { time: '16 hours ago', title: 'The GitHub MCP Server adds support for tool…' },
+    { time: '16 hours ago', title: 'The GitHub MCP Server adds support for tool…' },
+    { time: '16 hours ago', title: 'The GitHub MCP Server adds support for tool…' }
+  ];
 }
